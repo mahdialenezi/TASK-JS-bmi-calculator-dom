30ad9239e47dfc155d5c419e56dcde48af12c463
mahdialenezi <mahdialmanhal32@gmail.com>
2024-10-29 20:16:23 +0300

    bmi

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  const BMI = ((weight) /(height/100)**2);
+  alert(BMI);
+
+  if (BMI < 18.5) {
+    alert("Underweight");
+  } else if (BMI >= 18.5 && BMI < 25) {
+    alert("Healthy weight");
+  } else if (BMI >= 25 && BMI < 30) {
+    alert("Overweight");
+  } else if  (BMI >= 30 ) {
+    alert("Obesity");
+  }
+
 }
